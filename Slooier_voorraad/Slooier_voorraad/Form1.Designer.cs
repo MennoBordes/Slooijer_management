62ece96a807df71982596b9a25f383ee87de69e3
Skillerde6de <mennobordes@hetnet.nl>
2019-02-09 19:32:24 +0100

    Added header image

--- Slooier_voorraad/Slooier_voorraad/Form1.Designer.cs
+++ Slooier_voorraad/Slooier_voorraad/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Slooier_voorraad
 {
-	partial class Form1
+	partial class MainPage
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,13 +28,41 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainPage));
+			this.PboxLogo = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.PboxLogo)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// PboxLogo
+			// 
+			this.PboxLogo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.PboxLogo.BackColor = System.Drawing.Color.White;
+			this.PboxLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("PboxLogo.BackgroundImage")));
+			this.PboxLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.PboxLogo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.PboxLogo.Location = new System.Drawing.Point(12, 12);
+			this.PboxLogo.Name = "PboxLogo";
+			this.PboxLogo.Size = new System.Drawing.Size(1160, 115);
+			this.PboxLogo.TabIndex = 1;
+			this.PboxLogo.TabStop = false;
+			// 
+			// MainPage
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(1184, 761);
+			this.Controls.Add(this.PboxLogo);
+			this.Name = "MainPage";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.PboxLogo)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.PictureBox PboxLogo;
 	}
 }
 
--- Slooier_voorraad/Slooier_voorraad/Program.cs
+++ Slooier_voorraad/Slooier_voorraad/Program.cs
@@ -16,7 +16,7 @@ namespace Slooier_voorraad
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new MainPage());
 		}
 	}
 }
