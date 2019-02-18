f9122a4d799c6c3380027d705fad41f31b352217
Skillerde6de <mennobordes@hetnet.nl>
2019-02-18 21:50:39 +0100

    Added common functions file

---
+++ Slooier_voorraad/Slooier_voorraad/Classes/CommonFunctions/CommonFunctions.cs
@@ -0,0 +1,13 @@
+﻿using Npgsql;
+using Slooier_voorraad.Classes.CustomMessageBox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Slooier_voorraad.Classes.CommonFunctions
+{
+	public class CommonFunctions
+	{
+	}
+}
