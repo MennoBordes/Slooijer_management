f2eacc4c48d7d2657f188347d62feba8537b4452
Skillerde6de <mennobordes@hetnet.nl>
2019-02-23 17:10:38 +0100

    Created new Starting screen

---
+++ Slooier_voorraad/Slooier_voorraad/NewStartingScreen.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Slooier_voorraad
+{
+	partial class NewStartingScreen
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewStartingScreen));
+			this.PboxLogo = new System.Windows.Forms.PictureBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.nieuweArtikelenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.instellingenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			((System.ComponentModel.ISupportInitialize)(this.PboxLogo)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// PboxLogo
+			// 
+			this.PboxLogo.BackColor = System.Drawing.Color.White;
+			this.PboxLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("PboxLogo.BackgroundImage")));
+			this.PboxLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.PboxLogo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.PboxLogo.Dock = System.Windows.Forms.DockStyle.Top;
+			this.PboxLogo.Location = new System.Drawing.Point(0, 24);
+			this.PboxLogo.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+			this.PboxLogo.Name = "PboxLogo";
+			this.PboxLogo.Size = new System.Drawing.Size(800, 108);
+			this.PboxLogo.TabIndex = 5;
+			this.PboxLogo.TabStop = false;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.nieuweArtikelenToolStripMenuItem,
+            this.instellingenToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 6;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// menuToolStripMenuItem
+			// 
+			this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+			this.menuToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+			this.menuToolStripMenuItem.Text = "&Voorraad";
+			// 
+			// nieuweArtikelenToolStripMenuItem
+			// 
+			this.nieuweArtikelenToolStripMenuItem.Name = "nieuweArtikelenToolStripMenuItem";
+			this.nieuweArtikelenToolStripMenuItem.Size = new System.Drawing.Size(109, 20);
+			this.nieuweArtikelenToolStripMenuItem.Text = "&Nieuwe Artikelen";
+			// 
+			// instellingenToolStripMenuItem
+			// 
+			this.instellingenToolStripMenuItem.Name = "instellingenToolStripMenuItem";
+			this.instellingenToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+			this.instellingenToolStripMenuItem.Text = "&Instellingen";
+			this.instellingenToolStripMenuItem.Click += new System.EventHandler(this.instellingenToolStripMenuItem_Click);
+			// 
+			// NewStartingScreen
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.PboxLogo);
+			this.Controls.Add(this.menuStrip1);
+			this.IsMdiContainer = true;
+			this.Name = "NewStartingScreen";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "NewStartingScreen";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			((System.ComponentModel.ISupportInitialize)(this.PboxLogo)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox PboxLogo;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem nieuweArtikelenToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem instellingenToolStripMenuItem;
+	}
+}
---
+++ Slooier_voorraad/Slooier_voorraad/NewStartingScreen.cs
@@ -0,0 +1,41 @@
+﻿using Slooier_voorraad.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slooier_voorraad
+{
+	public partial class NewStartingScreen : Form
+	{
+		public NewStartingScreen()
+		{
+			InitializeComponent();
+		}
+
+		private void instellingenToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			bool IsOpen = false;
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f.Text == "Instellingen")
+				{
+					IsOpen = true;
+					f.Focus();
+					break;
+				}
+			}
+			if (IsOpen == false)
+			{
+				SettingForm f = new SettingForm();
+				f.MdiParent = this;
+				f.Show();
+			}
+		}
+	}
+}
