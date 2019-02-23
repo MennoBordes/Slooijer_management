976e943c25a7570471182ba0e68aa8ff610e9743
Skillerde6de <mennobordes@hetnet.nl>
2019-02-23 17:10:04 +0100

    Removed image

--- Slooier_voorraad/Slooier_voorraad/Forms/SettingForm.Designer.cs
+++ Slooier_voorraad/Slooier_voorraad/Forms/SettingForm.Designer.cs
@@ -28,38 +28,21 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingForm));
-			this.PboxLogo = new System.Windows.Forms.PictureBox();
 			this.PMain = new System.Windows.Forms.Panel();
 			this.GbDB = new System.Windows.Forms.GroupBox();
-			this.LblServer = new System.Windows.Forms.Label();
 			this.LblUserName = new System.Windows.Forms.Label();
-			((System.ComponentModel.ISupportInitialize)(this.PboxLogo)).BeginInit();
+			this.LblServer = new System.Windows.Forms.Label();
 			this.PMain.SuspendLayout();
 			this.GbDB.SuspendLayout();
 			this.SuspendLayout();
-			// 
-			// PboxLogo
-			// 
-			this.PboxLogo.BackColor = System.Drawing.Color.White;
-			this.PboxLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("PboxLogo.BackgroundImage")));
-			this.PboxLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-			this.PboxLogo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.PboxLogo.Dock = System.Windows.Forms.DockStyle.Top;
-			this.PboxLogo.Location = new System.Drawing.Point(0, 0);
-			this.PboxLogo.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
-			this.PboxLogo.Name = "PboxLogo";
-			this.PboxLogo.Size = new System.Drawing.Size(800, 108);
-			this.PboxLogo.TabIndex = 4;
-			this.PboxLogo.TabStop = false;
 			// 
 			// PMain
 			// 
 			this.PMain.Controls.Add(this.GbDB);
 			this.PMain.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.PMain.Location = new System.Drawing.Point(0, 108);
+			this.PMain.Location = new System.Drawing.Point(0, 0);
 			this.PMain.Name = "PMain";
-			this.PMain.Size = new System.Drawing.Size(800, 342);
+			this.PMain.Size = new System.Drawing.Size(800, 450);
 			this.PMain.TabIndex = 7;
 			// 
 			// GbDB
@@ -73,15 +56,6 @@
 			this.GbDB.TabStop = false;
 			this.GbDB.Text = "Database connectie";
 			// 
-			// LblServer
-			// 
-			this.LblServer.AutoSize = true;
-			this.LblServer.Location = new System.Drawing.Point(7, 20);
-			this.LblServer.Name = "LblServer";
-			this.LblServer.Size = new System.Drawing.Size(44, 13);
-			this.LblServer.TabIndex = 0;
-			this.LblServer.Text = "Server: ";
-			// 
 			// LblUserName
 			// 
 			this.LblUserName.AutoSize = true;
@@ -91,20 +65,26 @@
 			this.LblUserName.TabIndex = 1;
 			this.LblUserName.Text = "Gebruikers naam: ";
 			// 
+			// LblServer
+			// 
+			this.LblServer.AutoSize = true;
+			this.LblServer.Location = new System.Drawing.Point(7, 20);
+			this.LblServer.Name = "LblServer";
+			this.LblServer.Size = new System.Drawing.Size(44, 13);
+			this.LblServer.TabIndex = 0;
+			this.LblServer.Text = "Server: ";
+			// 
 			// SettingForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
 			this.Controls.Add(this.PMain);
-			this.Controls.Add(this.PboxLogo);
 			this.Name = "SettingForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Instellingen";
-			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
 			this.Load += new System.EventHandler(this.SettingForm_Load);
 			this.SizeChanged += new System.EventHandler(this.SettingForm_SizeChanged);
-			((System.ComponentModel.ISupportInitialize)(this.PboxLogo)).EndInit();
 			this.PMain.ResumeLayout(false);
 			this.GbDB.ResumeLayout(false);
 			this.GbDB.PerformLayout();
@@ -113,8 +93,6 @@
 		}
 
 		#endregion
-
-		private System.Windows.Forms.PictureBox PboxLogo;
 		private System.Windows.Forms.Panel PMain;
 		private System.Windows.Forms.GroupBox GbDB;
 		private System.Windows.Forms.Label LblServer;
--- Slooier_voorraad/Slooier_voorraad/Forms/VoorraadCorrectie.Designer.cs
+++ Slooier_voorraad/Slooier_voorraad/Forms/VoorraadCorrectie.Designer.cs
@@ -28,31 +28,15 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VoorraadCorrectie));
-			this.PboxLogo = new System.Windows.Forms.PictureBox();
 			this.PMain = new System.Windows.Forms.Panel();
-			((System.ComponentModel.ISupportInitialize)(this.PboxLogo)).BeginInit();
 			this.SuspendLayout();
-			// 
-			// PboxLogo
-			// 
-			this.PboxLogo.BackColor = System.Drawing.Color.White;
-			this.PboxLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("PboxLogo.BackgroundImage")));
-			this.PboxLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-			this.PboxLogo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.PboxLogo.Dock = System.Windows.Forms.DockStyle.Top;
-			this.PboxLogo.Location = new System.Drawing.Point(0, 0);
-			this.PboxLogo.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
-			this.PboxLogo.Name = "PboxLogo";
-			this.PboxLogo.Size = new System.Drawing.Size(800, 108);
-			this.PboxLogo.TabIndex = 4;
-			this.PboxLogo.TabStop = false;
 			// 
 			// PMain
 			// 
-			this.PMain.Location = new System.Drawing.Point(0, 112);
+			this.PMain.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.PMain.Location = new System.Drawing.Point(0, 0);
 			this.PMain.Name = "PMain";
-			this.PMain.Size = new System.Drawing.Size(800, 339);
+			this.PMain.Size = new System.Drawing.Size(800, 450);
 			this.PMain.TabIndex = 5;
 			this.PMain.SizeChanged += new System.EventHandler(this.PMain_SizeChanged);
 			// 
@@ -62,20 +46,16 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
 			this.Controls.Add(this.PMain);
-			this.Controls.Add(this.PboxLogo);
 			this.Name = "VoorraadCorrectie";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "VoorradCorrectie";
 			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
 			this.Load += new System.EventHandler(this.VoorraadCorrectie_Load);
-			((System.ComponentModel.ISupportInitialize)(this.PboxLogo)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
-
-		private System.Windows.Forms.PictureBox PboxLogo;
 		private System.Windows.Forms.Panel PMain;
 	}
 }
