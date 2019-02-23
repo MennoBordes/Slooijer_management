08b53505cdf65ebd671a61c91c7c235ab34f1349
Skillerde6de <mennobordes@hetnet.nl>
2019-02-23 10:59:58 +0100

    Created new window

---
+++ Slooier_voorraad/Slooier_voorraad/Forms/AddDataPopup/AddAfdelingPopup.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Slooier_voorraad.Forms.AddDataPopup
+{
+	partial class AddAfdelingPopup
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddAfdelingPopup));
+			this.PboxLogo = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.PboxLogo)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// PboxLogo
+			// 
+			this.PboxLogo.BackColor = System.Drawing.Color.White;
+			this.PboxLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("PboxLogo.BackgroundImage")));
+			this.PboxLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.PboxLogo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.PboxLogo.Dock = System.Windows.Forms.DockStyle.Top;
+			this.PboxLogo.Location = new System.Drawing.Point(0, 0);
+			this.PboxLogo.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+			this.PboxLogo.Name = "PboxLogo";
+			this.PboxLogo.Size = new System.Drawing.Size(800, 108);
+			this.PboxLogo.TabIndex = 6;
+			this.PboxLogo.TabStop = false;
+			// 
+			// AddAfdelingPopup
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.PboxLogo);
+			this.Name = "AddAfdelingPopup";
+			this.Text = "AddAfdelingPopup";
+			((System.ComponentModel.ISupportInitialize)(this.PboxLogo)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox PboxLogo;
+	}
+}
---
+++ Slooier_voorraad/Slooier_voorraad/Forms/AddDataPopup/AddAfdelingPopup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slooier_voorraad.Forms.AddDataPopup
+{
+	public partial class AddAfdelingPopup : Form
+	{
+		string ConnString;
+		public AddAfdelingPopup(string ConnString)
+		{
+			InitializeComponent();
+			this.ConnString = ConnString;
+		}
+	}
+}
