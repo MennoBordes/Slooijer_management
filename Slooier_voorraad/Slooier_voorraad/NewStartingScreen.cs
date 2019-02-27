47abce8e73411f2306277a881bc47d0c974e4d62
Skillerde6de <mennobordes@hetnet.nl>
2019-02-27 16:19:04 +0100

    Inserter of new data file

--- Slooier_voorraad/Slooier_voorraad/NewStartingScreen.cs
+++ Slooier_voorraad/Slooier_voorraad/NewStartingScreen.cs
@@ -1,6 +1,12 @@
-﻿using Slooier_voorraad.Forms;
+﻿using Npgsql;
+using NpgsqlTypes;
+using Slooier_voorraad.Classes.CustomMessageBox;
+using Slooier_voorraad.Forms;
 using Slooier_voorraad.Forms.AddDataPopup;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -118,12 +124,154 @@ namespace Slooier_voorraad
       List<string> addedData = new List<string>();
       try
       {
+        var File = FilePath;
+        string afdelingNaam = string.Empty;
+        using (var reader = new StreamReader(File))
+        {
+          while (!reader.EndOfStream)
+          {
+            var line = reader.ReadLine();
+            var values = line.Split(';');
+            if(values[0]!= "")
+            {
+              afdelingNaam = values[0].ToLower();
+              using(var conn = new NpgsqlConnection(ConnString))
+              {
+                conn.Open();
+                bool exists = false;
+                using(var cmd = new NpgsqlCommand())
+                {
+                  cmd.Connection = conn;
+                  cmd.CommandText = string.Format(@"SELECT afdelingnaam FROM afdelingen WHERE afdelingnaam = @naam");
+                  cmd.Parameters.AddWithValue("naam", afdelingNaam);
+                  using(var SqlReader = cmd.ExecuteReader())
+                  {
+                    while (SqlReader.Read())
+                    {
+                      if(SqlReader.GetString(0).ToLower() == afdelingNaam)
+                      {
+                        exists = true;
+                        break;
+                      }
+                    }
+                  }
+                }
+                if (!exists)
+                {
+                  using(var cmd = new NpgsqlCommand())
+                  {
+                    cmd.Connection = conn;
+                    cmd.CommandText = string.Format(@"INSERT INTO afdelingen(afdelingnaam) VALUES(@NieuweNaam);");
+                    cmd.Parameters.AddWithValue("NieuweNaam", afdelingNaam);
+                    cmd.ExecuteNonQuery();
+                  }
+                  addedData.Add(afdelingNaam);
+                }
+              }
+            }
+            if(values[1] != "" || values[3] != "")
+            {
+              using(var conn = new NpgsqlConnection(ConnString))
+              {
+                conn.Open();
+                bool exists = false;
+                using(var cmd = new NpgsqlCommand())
+                {
+                  cmd.Connection = conn;
+                  cmd.CommandText = string.Format(@"SELECT nummer, omschrijving FROM voorraad WHERE nummer = @nummer AND omschrijving = @omschrijving;");
+                  cmd.Parameters.AddWithValue("nummer", values[1]);
+                  cmd.Parameters.AddWithValue("omschrijving", values[3]);
+                  using(var SqlReader = cmd.ExecuteReader())
+                  {
+                    while (SqlReader.Read())
+                    {
+                      string res1 = SqlReader.GetString(0).ToLower();
+                      string res2 = SqlReader.GetString(1).ToLower();
+                      if(res1 == values[1].ToLower() && res2 == values[3].ToLower())
+                      {
+                        exists = true;
+                        break;
+                      }
+                    }
+                  }
+                }
+                if (!exists)
+                {
+                  int AfdelingId = int.MinValue;
+                  using(var cmd = new NpgsqlCommand())
+                  {
+                    cmd.Connection = conn;
+                    cmd.CommandText = string.Format(@"SELECT id FROM afdelingen WHERE afdelingnaam = @afdelingnaam;");
+                    cmd.Parameters.AddWithValue("afdelingnaam", afdelingNaam);
+                    using (var SqlReader = cmd.ExecuteReader())
+                    {
+                      while (SqlReader.Read())
+                      {
+                        AfdelingId = SqlReader.GetInt32(0);
+                      }
+                    }
+                  }
+                  if(AfdelingId != int.MinValue)
+                  {
+                    string Nummer = values[1];
+                    string Omschrijving = values[3];
+                    int Voorraad = 0;
+
+                    string PrijsInput = values[4];
+                    
+                    double temp = 0.0;
+                    
+                    // ############################
+                    // TODO: Testen zonder bestaande DB
+                    double Prijs = GetDouble(PrijsInput, temp);
+
+                    //continue;
+
+                    using (var cmd = new NpgsqlCommand())
+                    {
+                      cmd.Connection = conn;
+                      cmd.CommandText = string.Format(@"INSERT INTO voorraad(nummer, omschrijving, voorraad, afdeling, prijs) VALUES(@Nummer, @Omschrijving, @Voorraad, @AfdelingId, @Prijs);");
+                      var ParNum = new NpgsqlParameter("Nummer", NpgsqlDbType.Text) { Value = Nummer };
+                      cmd.Parameters.Add(ParNum);
+                      var ParOms = new NpgsqlParameter("Omschrijving", NpgsqlDbType.Text) { Value = Omschrijving };
+                      cmd.Parameters.Add(ParOms);
+                      var ParVoo = new NpgsqlParameter("Voorraad", NpgsqlDbType.Integer) { Value = Voorraad };
+                      cmd.Parameters.Add(ParVoo);
+                      var ParAfd = new NpgsqlParameter("AfdelingId", NpgsqlDbType.Integer) { Value = AfdelingId };
+                      cmd.Parameters.Add(ParAfd);
+                      var ParPri = new NpgsqlParameter("Prijs", NpgsqlDbType.Double) { Value = Prijs };
+                      cmd.Parameters.Add(ParPri);
+                      cmd.Prepare();
+                      cmd.ExecuteNonQuery();
+                      // TODO
+                      // afmaken volgens AddItemPopup en AddOrRemoveItems
+                    }
+                    addedData.Add($"{values[1]} {values[3]}");
+                  }
+                }
+              }
+            }
+          }
+        }
       }
       catch (Exception ex)
       {
         FlexibleMessageBox.Show(ex.Message);
       }
       return addedData;
+    }
+
+    private static double GetDouble(string value, double defaultValue)
+    {
+      double result;
+
+      if(!double.TryParse(value,System.Globalization.NumberStyles.Any,CultureInfo.CurrentCulture, out result) &&
+        !double.TryParse(value, System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out result) &&
+        !double.TryParse(value, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out result))
+      {
+        result = defaultValue;
+      }
+      return result;
     }
 
 		private void NewStartingScreen_Load(object sender, EventArgs e)
