cb4fa8acbfb2d80296254a4ce3d061b1f0538cd4
Skillerde6de <mennobordes@hetnet.nl>
2019-02-28 12:44:18 +0100

    Update StartingScreenFunctions.cs

--- Slooier_voorraad/Slooier_voorraad/Classes/StartingScreenFunctions/StartingScreenFunctions.cs
+++ Slooier_voorraad/Slooier_voorraad/Classes/StartingScreenFunctions/StartingScreenFunctions.cs
@@ -6,4 +6,17 @@ using System.Threading.Tasks;
 
 namespace Slooier_voorraad.Classes.StartingScreenFunctions
 {
+	static class StartingScreenFunctions
+	{
+		public static double GetDouble(string value, double defaultValue)
+		{
+			if (!double.TryParse(value, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.CurrentCulture, out double result) &&
+				!double.TryParse(value, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.GetCultureInfo("en-US"), out result) &&
+				!double.TryParse(value, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out result))
+			{
+				result = defaultValue;
+			}
+			return result;
+		}
+	}
 }
