cbb6e572e845278db8e3d446a9adb18013861202
Skillerde6de <mennobordes@hetnet.nl>
2019-03-01 16:59:36 +0100

    Minor functionalities for the Trial Wpf

--- Slooier_voorraad/Slooier_Voorraad_WPF/MainWindow.xaml.cs
+++ Slooier_voorraad/Slooier_Voorraad_WPF/MainWindow.xaml.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Slooier_Voorraad_WPF
 {
+	//https://www.youtube.com/watch?v=6OwyNiLPDNw&list=PLrW43fNmjaQVYF4zgsD0oL9Iv6u23PI6M&index=2
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
@@ -23,6 +12,42 @@ namespace Slooier_Voorraad_WPF
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			FinishDropDown_SelectionChanged(this.FinishDropDown, null);
+		}
+
+		private void ApplyButton_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show($"The description is: {DescriptionText.Text}");
+		}
+
+		private void ResetButton_Click(object sender, RoutedEventArgs e)
+		{
+			WeldCheckbox.IsChecked = AssemblyCheckbox.IsChecked = PlasmaCheckbox.IsChecked = LaserCheckbox.IsChecked = PurchaseCheckbox.IsChecked = LatheCheckbox.IsChecked = DrillCheckbox.IsChecked = FoldCheckbox.IsChecked = RollCheckbox.IsChecked = SawCheckbox.IsChecked = false;
+		}
+
+		private void Checkbox_Checked(object sender, RoutedEventArgs e)
+		{
+			LengthText.Text += ((CheckBox)sender).Content;
+		}
+
+		private void FinishDropDown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (NoteText == null)
+				return;
+
+			var combo = (ComboBox)sender;
+			var value = (ComboBoxItem)combo.SelectedValue;
+
+			NoteText.Text = (string)value.Content;
+		}
+
+		private void SupplierNameText_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			this.MassText.Text = this.SupplierNameText.Text;
 		}
 	}
 }
