ba95c5e7331f7b1096c081bc52d06b08b76bf341
Skillerde6de <mennobordes@hetnet.nl>
2019-03-03 21:50:57 +0100

    Simple datagrid filler

--- Slooier_voorraad/Slooier_Voorraad_WPF/Window2.xaml.cs
+++ Slooier_voorraad/Slooier_Voorraad_WPF/Window2.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Npgsql;
+using System.Data;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Slooier_Voorraad_WPF
 {
@@ -22,6 +12,26 @@ namespace Slooier_Voorraad_WPF
 		public Window2()
 		{
 			InitializeComponent();
+		}
+		public string ConnString = string.Format($"Server=localhost; User Id=postgres; Database =Slooier_VoorraadSysteem; Port=5432; Password=2761");
+		private void ButtonTester_Click(object sender, RoutedEventArgs e)
+		{
+			using (var conn = new NpgsqlConnection(ConnString))
+			{
+				conn.Open();
+				string command = string.Format("SELECT voorraad.id, afdelingnaam, nummer, omschrijving, voorraad, prijs " +
+													"FROM voorraad INNER JOIN afdelingen ON (voorraad.afdeling = afdelingen.id) ORDER BY afdelingnaam, omschrijving;");
+				using (var cmd = new NpgsqlCommand(command, conn))
+				{
+					cmd.ExecuteNonQuery();
+
+					NpgsqlDataAdapter dataAdp = new NpgsqlDataAdapter(cmd);
+					DataTable dt = new DataTable("afdelingen");
+					dataAdp.Fill(dt);
+					DataGridTester.ItemsSource = dt.DefaultView;
+					dataAdp.Update(dt);
+				}
+			}
 		}
 	}
 }
