d4085060cee99808651c46f827a970257cdda47e
Skillerde6de <mennobordes@hetnet.nl>
2019-03-04 14:25:31 +0100

    Startup for dashboard as from YT
    https://www.youtube.com/watch?v=sgEhK3mbDYo

---
+++ Slooier_voorraad/Slooier_Voorraad_WPF/Window3.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Slooier_Voorraad_WPF
+{
+    /// <summary>
+    /// Interaction logic for Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+    private void ButtonPower_Click(object sender, RoutedEventArgs e)
+    {
+      Application.Current.Shutdown();
+    }
+
+    private void GridBarTitle_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      DragMove();
+    }
+  }
+}
