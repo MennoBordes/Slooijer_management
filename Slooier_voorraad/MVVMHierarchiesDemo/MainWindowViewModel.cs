31276e8f66261ace5ddf3ec5331c2909926264d9
Skillerde6de <mennobordes@hetnet.nl>
2019-03-05 16:57:57 +0100

    MVVM Hierarchy attempt 1

---
+++ Slooier_voorraad/MVVMHierarchiesDemo/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MVVMHierarchiesDemo.ViewModel;
+using MVVMHierarchiesDemo.Views;
+
+namespace MVVMHierarchiesDemo
+{
+	class MainWindowViewModel : BindableBase
+	{
+		public MainWindowViewModel()
+		{
+			NavCommand = new MyICommand<string>(OnNav);
+		}
+
+		private CustomerListViewModel custListViewModel = new CustomerListViewModel();
+
+		private OrderViewModel orderViewModelModel = new OrderViewModel();
+
+		private BindableBase _CurrentViewModel;
+
+		public BindableBase CurrentViewModel
+		{
+			get { return _CurrentViewModel;}
+			set { SetProperty(ref _CurrentViewModel, value); }
+		}
+
+		public MyICommand<string> NavCommand { get; private set; }
+
+		private void OnNav(string destination)
+		{
+			switch (destination)
+			{
+				case "orders":
+					CurrentViewModel = orderViewModelModel;
+					break;
+				case "customers":
+					CurrentViewModel = custListViewModel;
+					break;
+			}
+		}
+	}
+}
