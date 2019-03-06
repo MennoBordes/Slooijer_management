96253ba6e86408a3ba1700c91779d4a32ec6e28e
Skillerde6de <mennobordes@hetnet.nl>
2019-03-06 22:01:17 +0100

    Removed excess dependencies

--- Slooier_voorraad/Slooier_voorraad/Forms/DeleteDataPopup/DeleteItemPopup.cs
+++ Slooier_voorraad/Slooier_voorraad/Forms/DeleteDataPopup/DeleteItemPopup.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace Slooier_voorraad.Forms.DeleteDataPopup
@@ -16,5 +9,6 @@ namespace Slooier_voorraad.Forms.DeleteDataPopup
 		{
 			InitializeComponent();
 		}
+
 	}
 }
