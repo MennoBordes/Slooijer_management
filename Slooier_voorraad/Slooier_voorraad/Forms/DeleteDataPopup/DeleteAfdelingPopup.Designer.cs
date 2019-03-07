13f43ea960dcbe8d91073e0065ad7e4d86d81c60
Skillerde6de <mennobordes@hetnet.nl>
2019-03-07 14:25:44 +0100

    Changed form text

--- Slooier_voorraad/Slooier_voorraad/Forms/DeleteDataPopup/DeleteAfdelingPopup.Designer.cs
+++ Slooier_voorraad/Slooier_voorraad/Forms/DeleteDataPopup/DeleteAfdelingPopup.Designer.cs
@@ -28,31 +28,33 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.defaultBackground1 = new Slooier_voorraad.Controls.DefaultBackground();
-			this.SuspendLayout();
-			// 
-			// defaultBackground1
-			// 
-			this.defaultBackground1.AutoSize = true;
-			this.defaultBackground1.BackColor = System.Drawing.Color.Gainsboro;
-			this.defaultBackground1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.defaultBackground1.Location = new System.Drawing.Point(0, 0);
-			this.defaultBackground1.Name = "defaultBackground1";
-			this.defaultBackground1.Size = new System.Drawing.Size(800, 450);
-			this.defaultBackground1.TabIndex = 0;
-			// 
-			// DeleteAfdelingPopup
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.defaultBackground1);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.Name = "DeleteAfdelingPopup";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "DeleteAfdelingPopup";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+      this.defaultBackground1 = new Slooier_voorraad.Controls.DefaultBackground();
+      this.SuspendLayout();
+      // 
+      // defaultBackground1
+      // 
+      this.defaultBackground1.AutoSize = true;
+      this.defaultBackground1.BackColor = System.Drawing.Color.Gainsboro;
+      this.defaultBackground1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.defaultBackground1.Location = new System.Drawing.Point(0, 0);
+      this.defaultBackground1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+      this.defaultBackground1.Name = "defaultBackground1";
+      this.defaultBackground1.Size = new System.Drawing.Size(1067, 554);
+      this.defaultBackground1.TabIndex = 0;
+      // 
+      // DeleteAfdelingPopup
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1067, 554);
+      this.Controls.Add(this.defaultBackground1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.Name = "DeleteAfdelingPopup";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Afdeling Verwijderen";
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
 		}
 
