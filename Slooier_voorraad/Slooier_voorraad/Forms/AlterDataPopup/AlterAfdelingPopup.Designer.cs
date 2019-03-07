1596e47740f7642af2acb1ba4ca217ebf7ca18d0
Skillerde6de <mennobordes@hetnet.nl>
2019-03-07 22:32:50 +0100

    Removed excess code

--- Slooier_voorraad/Slooier_voorraad/Forms/AlterDataPopup/AlterAfdelingPopup.Designer.cs
+++ Slooier_voorraad/Slooier_voorraad/Forms/AlterDataPopup/AlterAfdelingPopup.Designer.cs
@@ -28,124 +28,175 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-      this.FlpNew = new System.Windows.Forms.FlowLayoutPanel();
-      this.label2 = new System.Windows.Forms.Label();
-      this.CbbHuidigeNaam = new System.Windows.Forms.ComboBox();
-      this.label3 = new System.Windows.Forms.Label();
-      this.TxbNewName = new System.Windows.Forms.TextBox();
-      this.BtnAlterAfdeling = new System.Windows.Forms.Button();
-      this.LblMain = new System.Windows.Forms.Label();
-      this.defaultBackground1 = new Slooier_voorraad.Controls.DefaultBackground();
-      this.FlpNew.SuspendLayout();
-      this.SuspendLayout();
-      // 
-      // FlpNew
-      // 
-      this.FlpNew.BackColor = System.Drawing.Color.Transparent;
-      this.FlpNew.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-      this.FlpNew.Controls.Add(this.label2);
-      this.FlpNew.Controls.Add(this.CbbHuidigeNaam);
-      this.FlpNew.Controls.Add(this.label3);
-      this.FlpNew.Controls.Add(this.TxbNewName);
-      this.FlpNew.Controls.Add(this.BtnAlterAfdeling);
-      this.FlpNew.Location = new System.Drawing.Point(99, 79);
-      this.FlpNew.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-      this.FlpNew.Name = "FlpNew";
-      this.FlpNew.Size = new System.Drawing.Size(322, 136);
-      this.FlpNew.TabIndex = 15;
-      // 
-      // label2
-      // 
-      this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(4, 0);
-      this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(302, 17);
-      this.label2.TabIndex = 6;
-      this.label2.Text = "Van welke afdeling wilt u de naam aanpassen?";
-      // 
-      // CbbHuidigeNaam
-      // 
-      this.CbbHuidigeNaam.FormattingEnabled = true;
-      this.CbbHuidigeNaam.Location = new System.Drawing.Point(4, 21);
-      this.CbbHuidigeNaam.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-      this.CbbHuidigeNaam.MaxDropDownItems = 20;
-      this.CbbHuidigeNaam.Name = "CbbHuidigeNaam";
-      this.CbbHuidigeNaam.Size = new System.Drawing.Size(317, 24);
-      this.CbbHuidigeNaam.TabIndex = 1;
-      this.CbbHuidigeNaam.Text = "Afdeling:";
-      // 
-      // label3
-      // 
-      this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point(4, 49);
-      this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-      this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(307, 17);
-      this.label3.TabIndex = 7;
-      this.label3.Text = "Welke naam moet de gekozen afdeling krijgen?";
-      // 
-      // TxbNewName
-      // 
-      this.TxbNewName.Location = new System.Drawing.Point(4, 70);
-      this.TxbNewName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-      this.TxbNewName.Name = "TxbNewName";
-      this.TxbNewName.Size = new System.Drawing.Size(317, 22);
-      this.TxbNewName.TabIndex = 2;
-      // 
-      // BtnAlterAfdeling
-      // 
-      this.BtnAlterAfdeling.Dock = System.Windows.Forms.DockStyle.Bottom;
-      this.BtnAlterAfdeling.Location = new System.Drawing.Point(4, 100);
-      this.BtnAlterAfdeling.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-      this.BtnAlterAfdeling.Name = "BtnAlterAfdeling";
-      this.BtnAlterAfdeling.Size = new System.Drawing.Size(319, 28);
-      this.BtnAlterAfdeling.TabIndex = 6;
-      this.BtnAlterAfdeling.Text = "Aanpassen";
-      this.BtnAlterAfdeling.UseVisualStyleBackColor = true;
-      // 
-      // LblMain
-      // 
-      this.LblMain.BackColor = System.Drawing.Color.Transparent;
-      this.LblMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.LblMain.Location = new System.Drawing.Point(13, 33);
-      this.LblMain.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-      this.LblMain.Name = "LblMain";
-      this.LblMain.Size = new System.Drawing.Size(500, 20);
-      this.LblMain.TabIndex = 2;
-      this.LblMain.Text = "Verander de naam van een afdeling";
-      this.LblMain.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-      // 
-      // defaultBackground1
-      // 
-      this.defaultBackground1.AutoSize = true;
-      this.defaultBackground1.BackColor = System.Drawing.Color.Gainsboro;
-      this.defaultBackground1.Dock = System.Windows.Forms.DockStyle.Fill;
-      this.defaultBackground1.Location = new System.Drawing.Point(0, 0);
-      this.defaultBackground1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-      this.defaultBackground1.Name = "defaultBackground1";
-      this.defaultBackground1.Size = new System.Drawing.Size(527, 465);
-      this.defaultBackground1.TabIndex = 17;
-      // 
-      // AlterAfdelingPopup
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(527, 465);
-      this.Controls.Add(this.LblMain);
-      this.Controls.Add(this.FlpNew);
-      this.Controls.Add(this.defaultBackground1);
-      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-      this.Name = "AlterAfdelingPopup";
-      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-      this.Text = "Afdeling Aanpassen";
-      this.Load += new System.EventHandler(this.AlterAfdelingPopup_Load);
-      this.SizeChanged += new System.EventHandler(this.AlterAfdelingPopup_SizeChanged);
-      this.FlpNew.ResumeLayout(false);
-      this.FlpNew.PerformLayout();
-      this.ResumeLayout(false);
-      this.PerformLayout();
+			this.FlpNew = new System.Windows.Forms.FlowLayoutPanel();
+			this.label2 = new System.Windows.Forms.Label();
+			this.CbbHuidigeNaam = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.TxbNewName = new System.Windows.Forms.TextBox();
+			this.BtnAlterAfdeling = new System.Windows.Forms.Button();
+			this.LblMain = new System.Windows.Forms.Label();
+			this.BottomBorder = new Slooier_voorraad.UI.OuterFormBorder();
+			this.RightBorder = new Slooier_voorraad.UI.OuterFormBorder();
+			this.LeftBorder = new Slooier_voorraad.UI.OuterFormBorder();
+			this.defaultBackground1 = new Slooier_voorraad.Controls.DefaultHeaderBar();
+			this.defaultBackGround2 = new Slooier_voorraad.UI.DefaultBackGround();
+			this.FlpNew.SuspendLayout();
+			this.defaultBackGround2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// FlpNew
+			// 
+			this.FlpNew.BackColor = System.Drawing.Color.Transparent;
+			this.FlpNew.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.FlpNew.Controls.Add(this.label2);
+			this.FlpNew.Controls.Add(this.CbbHuidigeNaam);
+			this.FlpNew.Controls.Add(this.label3);
+			this.FlpNew.Controls.Add(this.TxbNewName);
+			this.FlpNew.Controls.Add(this.BtnAlterAfdeling);
+			this.FlpNew.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+			this.FlpNew.Location = new System.Drawing.Point(103, 81);
+			this.FlpNew.Name = "FlpNew";
+			this.FlpNew.Size = new System.Drawing.Size(242, 111);
+			this.FlpNew.TabIndex = 15;
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(3, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(232, 13);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Van welke afdeling wilt u de naam aanpassen?";
+			// 
+			// CbbHuidigeNaam
+			// 
+			this.CbbHuidigeNaam.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.CbbHuidigeNaam.FormattingEnabled = true;
+			this.CbbHuidigeNaam.Location = new System.Drawing.Point(3, 16);
+			this.CbbHuidigeNaam.MaxDropDownItems = 20;
+			this.CbbHuidigeNaam.Name = "CbbHuidigeNaam";
+			this.CbbHuidigeNaam.Size = new System.Drawing.Size(232, 21);
+			this.CbbHuidigeNaam.TabIndex = 1;
+			this.CbbHuidigeNaam.Text = "Afdeling:";
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(3, 40);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(232, 13);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Welke naam moet de gekozen afdeling krijgen?";
+			// 
+			// TxbNewName
+			// 
+			this.TxbNewName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.TxbNewName.Location = new System.Drawing.Point(3, 56);
+			this.TxbNewName.Name = "TxbNewName";
+			this.TxbNewName.Size = new System.Drawing.Size(232, 20);
+			this.TxbNewName.TabIndex = 2;
+			// 
+			// BtnAlterAfdeling
+			// 
+			this.BtnAlterAfdeling.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.BtnAlterAfdeling.Location = new System.Drawing.Point(3, 82);
+			this.BtnAlterAfdeling.Name = "BtnAlterAfdeling";
+			this.BtnAlterAfdeling.Size = new System.Drawing.Size(232, 23);
+			this.BtnAlterAfdeling.TabIndex = 6;
+			this.BtnAlterAfdeling.Text = "Aanpassen";
+			this.BtnAlterAfdeling.UseVisualStyleBackColor = true;
+			// 
+			// LblMain
+			// 
+			this.LblMain.BackColor = System.Drawing.Color.Transparent;
+			this.LblMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.LblMain.Location = new System.Drawing.Point(100, 62);
+			this.LblMain.Name = "LblMain";
+			this.LblMain.Size = new System.Drawing.Size(255, 16);
+			this.LblMain.TabIndex = 2;
+			this.LblMain.Text = "Verander de naam van een afdeling";
+			this.LblMain.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// BottomBorder
+			// 
+			this.BottomBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(57)))), ((int)(((byte)(182)))));
+			this.BottomBorder.Cursor = System.Windows.Forms.Cursors.SizeNS;
+			this.BottomBorder.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.BottomBorder.Location = new System.Drawing.Point(8, 340);
+			this.BottomBorder.Name = "BottomBorder";
+			this.BottomBorder.OutBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(57)))), ((int)(((byte)(182)))));
+			this.BottomBorder.Size = new System.Drawing.Size(435, 8);
+			this.BottomBorder.TabIndex = 19;
+			// 
+			// RightBorder
+			// 
+			this.RightBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(57)))), ((int)(((byte)(182)))));
+			this.RightBorder.Cursor = System.Windows.Forms.Cursors.SizeWE;
+			this.RightBorder.Dock = System.Windows.Forms.DockStyle.Right;
+			this.RightBorder.Location = new System.Drawing.Point(443, 0);
+			this.RightBorder.Name = "RightBorder";
+			this.RightBorder.OutBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(57)))), ((int)(((byte)(182)))));
+			this.RightBorder.Size = new System.Drawing.Size(8, 348);
+			this.RightBorder.TabIndex = 18;
+			// 
+			// LeftBorder
+			// 
+			this.LeftBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(57)))), ((int)(((byte)(182)))));
+			this.LeftBorder.Cursor = System.Windows.Forms.Cursors.SizeWE;
+			this.LeftBorder.Dock = System.Windows.Forms.DockStyle.Left;
+			this.LeftBorder.Location = new System.Drawing.Point(0, 0);
+			this.LeftBorder.Name = "LeftBorder";
+			this.LeftBorder.OutBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(57)))), ((int)(((byte)(182)))));
+			this.LeftBorder.Size = new System.Drawing.Size(8, 348);
+			this.LeftBorder.TabIndex = 17;
+			// 
+			// defaultBackground1
+			// 
+			this.defaultBackground1.AutoSize = true;
+			this.defaultBackground1.BackColor = System.Drawing.Color.Gainsboro;
+			this.defaultBackground1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.defaultBackground1.Location = new System.Drawing.Point(8, 0);
+			this.defaultBackground1.Name = "defaultBackground1";
+			this.defaultBackground1.Size = new System.Drawing.Size(435, 27);
+			this.defaultBackground1.TabIndex = 16;
+			// 
+			// defaultBackGround2
+			// 
+			this.defaultBackGround2.BackColor = System.Drawing.Color.Gainsboro;
+			this.defaultBackGround2.Controls.Add(this.FlpNew);
+			this.defaultBackGround2.Controls.Add(this.LblMain);
+			this.defaultBackGround2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.defaultBackGround2.Location = new System.Drawing.Point(8, 27);
+			this.defaultBackGround2.Name = "defaultBackGround2";
+			this.defaultBackGround2.Size = new System.Drawing.Size(435, 313);
+			this.defaultBackGround2.TabIndex = 20;
+			// 
+			// AlterAfdelingPopup
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(451, 348);
+			this.Controls.Add(this.defaultBackGround2);
+			this.Controls.Add(this.defaultBackground1);
+			this.Controls.Add(this.BottomBorder);
+			this.Controls.Add(this.LeftBorder);
+			this.Controls.Add(this.RightBorder);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "AlterAfdelingPopup";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Afdeling Aanpassen";
+			this.FlpNew.ResumeLayout(false);
+			this.FlpNew.PerformLayout();
+			this.defaultBackGround2.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -157,6 +208,10 @@
 		private System.Windows.Forms.Label LblMain;
 		private System.Windows.Forms.FlowLayoutPanel FlpNew;
 		private System.Windows.Forms.Button BtnAlterAfdeling;
-    private Controls.DefaultBackground defaultBackground1;
-  }
+		private Controls.DefaultHeaderBar defaultBackground1;
+		private UI.OuterFormBorder LeftBorder;
+		private UI.OuterFormBorder RightBorder;
+		private UI.OuterFormBorder BottomBorder;
+		private UI.DefaultBackGround defaultBackGround2;
+	}
 }
