5db22bde030ea13b7103d46b1ee6925ec3178aff
Skillerde6de <mennobordes@hetnet.nl>
2019-03-07 22:33:43 +0100

    Misc

---
+++ Slooier_voorraad/Slooier_voorraad/UI/DefaultBackGround.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Slooier_voorraad.UI
+{
+	class DefaultBackGround : Panel
+	{
+		public DefaultBackGround()
+		{
+			BackColor = Properties.Settings.Default.BackGroundColor;
+		}
+	}
+}
