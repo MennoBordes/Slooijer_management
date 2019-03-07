e436345fb994cee3b77d707bc80160342377651b
Skillerde6de <mennobordes@hetnet.nl>
2019-03-07 13:19:34 +0100

    Implemented default background on settings form

--- Slooier_voorraad/Slooier_voorraad/Forms/SettingForm.cs
+++ Slooier_voorraad/Slooier_voorraad/Forms/SettingForm.cs
@@ -12,15 +12,15 @@ namespace Slooier_voorraad.Forms
 
 		private void SettingForm_SizeChanged(object sender, EventArgs e)
 		{
-			// Set panels to center of the Form
-			PMain.Left = (ClientSize.Width - PMain.Width) / 2;
-			PMain.Top = (ClientSize.Height - PMain.Height) / 2;
+			//// Set panels to center of the Form
+			//PMain.Left = (ClientSize.Width - PMain.Width) / 2;
+			//PMain.Top = (ClientSize.Height - PMain.Height) / 2;
 		}
 
 		private void SettingForm_Load(object sender, EventArgs e)
 		{
 			// Set minimumsize
-			MinimumSize = new System.Drawing.Size(Properties.Settings.Default.MinimumSizeX, Properties.Settings.Default.MinimumSizeY);
+			//MinimumSize = new System.Drawing.Size(Properties.Settings.Default.MinimumSizeX, Properties.Settings.Default.MinimumSizeY);
 			//Location = new System.Drawing.Point(Properties.Settings.Default.PX, Properties.Settings.Default.PY);
 			BackColor = Properties.Settings.Default.BackGroundColor;
 		}
